213d96b42588c38d0b92eac208654d2e0dbe9181
vaithorat <vaibhavthorat141997@gmail.com>
2022-10-15 15:43:11 +0530

    finished

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Meme from './components/Main';
+import Nav from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+   <Nav></Nav>
+   <Meme></Meme>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <header className="header">
+      <img className="header--image" src="./icons/Troll.png" alt="logo" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
